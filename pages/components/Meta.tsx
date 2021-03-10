ca6886d544255d182ff259cb7211d4151df46864
Patrik Mäsiar <patrik@kontentino.com>
2021-03-10 22:12:32 +0100

    Add google analytics script

--- pages/components/Meta.tsx
+++ pages/components/Meta.tsx
@@ -1,10 +1,24 @@
 import { FC } from 'react';
 import Head from 'next/head'
 
+const setGoogleTags = () => {
+  return {
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-NY3SNH44XP');
+    `
+  };
+};
+
 const Meta: FC = () => (
   <Head>
     <title>What key did I press | for developers</title>
     <link rel="icon" href="/favicon.ico" />
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NY3SNH44XP" />
+    <script dangerouslySetInnerHTML={setGoogleTags()} />
   </Head>
 );
 
