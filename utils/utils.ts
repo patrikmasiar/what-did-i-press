a3d5bad287f4b0165b08f51e691f7401edc1a853
massoprod <masiar.patrik@gmail.com>
2021-03-11 22:07:08 +0100

    Use try catch branch for formatter

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,36 +1,40 @@
 export const formatReadableEvent = (e: KeyboardEvent | null): string => {
-  if (e === null) {
+  try {
+    if (e === null) {
+      return JSON.stringify({});
+    }
+  
+    return JSON.stringify({KeyboardEvent: {
+      altKey: e.altKey,
+      bubbles: e.bubbles,
+      cancelBubble: e.cancelBubble,
+      cancelable: e.cancelable,
+      charCode: e.charCode,
+      code: e.code,
+      composed: e.composed,
+      ctrlKey: e.ctrlKey,
+      currentTarget: e.currentTarget,
+      defaultPrevented: e.defaultPrevented,
+      detail: e.detail,
+      eventPhase: e.eventPhase,
+      isComposing: e.isComposing,
+      isTrusted: e.isTrusted,
+      key: e.key,
+      keyCode: e.keyCode,
+      location: e.location,
+      metaKey: e.metaKey,
+      repeat: e.repeat,
+      returnValue: e.returnValue,
+      shiftKey: e.shiftKey,
+      srcElement: e.srcElement,
+      target: e.target,
+      timeStamp: e.timeStamp,
+      type: e.type,
+      which: e.which,
+      }}, null, 4);
+  } catch (e) {
     return JSON.stringify({});
   }
-
-  return JSON.stringify({KeyboardEvent: {
-    altKey: e.altKey,
-    bubbles: e.bubbles,
-    cancelBubble: e.cancelBubble,
-    cancelable: e.cancelable,
-    charCode: e.charCode,
-    code: e.code,
-    composed: e.composed,
-    ctrlKey: e.ctrlKey,
-    currentTarget: e.currentTarget,
-    defaultPrevented: e.defaultPrevented,
-    detail: e.detail,
-    eventPhase: e.eventPhase,
-    isComposing: e.isComposing,
-    isTrusted: e.isTrusted,
-    key: e.key,
-    keyCode: e.keyCode,
-    location: e.location,
-    metaKey: e.metaKey,
-    repeat: e.repeat,
-    returnValue: e.returnValue,
-    shiftKey: e.shiftKey,
-    srcElement: e.srcElement,
-    target: e.target,
-    timeStamp: e.timeStamp,
-    type: e.type,
-    which: e.which,
-    }}, null, 4);
 };
 
 // Used source from: http://jsfiddle.net/KJQ9K/554/
